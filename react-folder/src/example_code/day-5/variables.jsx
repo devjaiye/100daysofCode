ac0a98e366f7ada2f06908aa889ae7ec7bc358ce
devjaiye <dayojaiye74@gmail.com>
2022-08-25 22:49:28 +0100

    variables - day 5

--- react-folder/src/example_code/day-5/variables.jsx
+++ react-folder/src/example_code/day-5/variables.jsx
@@ -1,3 +1,22 @@
 //..Now, with ES6, there are three ways of defining your
 // variables: var, let, and const.
 
+/*
+var 
+•	If you use var outside of a function, it belongs to the global scope.
+•	If you use var inside of a function, it belongs to that function.
+•	If you use var inside of a block, i.e. a for loop, the variable is still available outside of that block.
+•	var has a function scope, not a block scope.
+let
+•	let is the block scoped version of var, and is limited to the block (or expression) where it is defined.
+•	If you use let inside of a block, i.e. a for loop, the variable is only available inside of that loop.
+•	let has a block scope.
+const
+•	const is a variable that once it has been created, its value can never change.
+•	const has a block scope.
+•	It does not define a constant value. It defines a constant reference to a value… For example, you can change the element of a constant array or the properties of a constant object. 
+You can NOT:
+•	Reassign a constant value
+•	Reassign a constant array
+•	Reassign a constant object
+*/
