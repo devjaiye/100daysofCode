57a717870a503c671ed16a963b6985f12a1df424
devjaiye <dayojaiye74@gmail.com>
2022-08-27 22:54:34 +0100

    Spread Operator in Array - day 7

--- react-folder/src/example_code/day-7/spread-operator.js
+++ react-folder/src/example_code/day-7/spread-operator.js
@@ -1,3 +1,9 @@
-//..It makes it easy to for us to combine all or part of
+//..Spread Operator (...) makes it easy to for us to combine all or part of
 //.. the element of an array or object into another
 
+const fruits= ['orange', 'apple', 'mango', 'banana']
+const number = [1,2,3,4,5,6,7]
+
+spreadArray = [...fruits, ...number]
+
+console.log(spreadArray)
