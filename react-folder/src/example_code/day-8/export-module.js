e89f7a887c6bdbab22db90b66f1bb19f17dd6f06
GlobalMadeDAO <dayo@mademarketingagency.com>
2022-08-29 23:40:45 +0100

    Export Module - day 8

---
+++ react-folder/src/example_code/day-8/export-module.js
@@ -0,0 +1,11 @@
+//..You can only have one default export in a file.
+
+const info = () => {
+    const name = "John Doe"
+    const address = "Lagos, Nigeria"
+    const age = 50
+
+    return name + " lives in " + address + ". He is " + age + " years old."
+}
+
+export default info
---
+++ react-folder/src/example_code/day-8/import-module.js
@@ -0,0 +1,2 @@
+//..import from modules.js file 
+
