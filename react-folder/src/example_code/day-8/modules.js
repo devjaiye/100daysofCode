c6c0035ea63dbf7b718c6f1c2de667095a2fce20
GlobalMadeDAO <dayo@mademarketingagency.com>
2022-08-29 23:58:39 +0100

    inport module - day 8

--- react-folder/src/example_code/day-8/modules.js
+++ react-folder/src/example_code/day-8/modules.js
@@ -8,6 +8,9 @@ export const carName = "Toyota Camry"
 export const price = 20000
 
 //..2. Named - All at once at the bottom 
- const carName = "Toyota Camry"
- const price = 30000
- export {carName, price} 
+ 
+
+    const carName = "Toyota Camry"
+    const price = 30000
+    export {carName, price} 
+ 
