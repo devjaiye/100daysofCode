8c75388842227f505a10dc4187254a719e18161a
GlobalMadeDAO <dayo@mademarketingagency.com>
2022-08-29 23:18:01 +0100

    modules - day 8

---
+++ react-folder/src/example_code/day-8/modules.js
@@ -0,0 +1,10 @@
+//..Modules make it easier for us to separate our codes into .
+//..it uses the import and export statements...
+
+//..there are 2 types of export:
+
+//..1. Named - In-line Individually 
+export const carName = "Toyota Camry"
+export const price = 20000
+
+ 
