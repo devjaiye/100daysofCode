e34578382d30d690417cff1dd67df56d51a588ad
devjaiye <dayojaiye74@gmail.com>
2022-08-30 23:51:47 +0100

    Ternary Operator - day 9

---
+++ react-folder/src/example_code/day-9/ternary-operator.js
@@ -0,0 +1,14 @@
+//..I can say it's an advance way of using if/else statements 
+
+function checkedIn(){
+    console.log("Student is present...")
+}
+
+function notCheckedIn(){
+    console.log("Student is absent...")
+}
+
+const checker = false
+
+//..ternary operator 
+checkedIn() ? notCheckedIn() : checker
