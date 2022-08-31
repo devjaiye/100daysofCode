0745306877948df4933c0911c5b411ce9ad2e897
devjaiye <dayojaiye74@gmail.com>
2022-08-31 23:49:57 +0100

    React Render HTML exp - day 10

--- react-folder/src/index.js
+++ react-folder/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const myfirstElement = <h1>Hello there</h1>
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(myfirstElement)
+// const myfirstElement = <h1>Hello there</h1>
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(myfirstElement)
+
+ReactDOM.render(<p>My React Render HTML doc</p>, document.getElementById('root'))
