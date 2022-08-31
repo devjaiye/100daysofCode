d797e97be222660ebe56ddc2b6c925349f041638
devjaiye <dayojaiye74@gmail.com>
2022-08-31 23:35:43 +0100

    React Render explained - day 10

--- react-folder/src/example_code/day-10/reactHtmlRender.jsx
+++ react-folder/src/example_code/day-10/reactHtmlRender.jsx
@@ -1,1 +1,7 @@
-//..React uses the function "ReactDOM.render()" to load in a web page
+//...React uses the function "ReactDOM.render()" to load in a web page
+//...The ReactDOM.render() function takes 2 arguments, 
+
+//. 1. HTML code and 2. an HTML element.
+
+//...The purpose of the function is to display the specified HTML 
+//...code inside the specified HTML element.
