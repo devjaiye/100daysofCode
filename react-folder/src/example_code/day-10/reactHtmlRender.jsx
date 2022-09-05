7111098e705a3857bb25c13fcc3a4333fc30e24e
devjaiye <dayojaiye74@gmail.com>
2022-09-05 22:52:39 +0100

    React Remder HTML ex2 - day 10

--- react-folder/src/example_code/day-10/reactHtmlRender.jsx
+++ react-folder/src/example_code/day-10/reactHtmlRender.jsx
@@ -7,3 +7,22 @@
 //...code inside the specified HTML element.
 
 //..our React code is rendered inside the index.html file which is found in the public folder
+
+import React from "react";
+
+const tableData = (
+    <table>
+        <tr>
+            <th>Classes</th>
+        </tr>
+        <tr>
+            <td>Class 2</td>
+        </tr>
+        <tr>
+            <td>Class 3</td>
+        </tr>
+    </table>
+)
+
+const root2 = ReactDOM.createRoot(document.getElementById('root'))
+root2.render(tableData)
--- react-folder/src/index.js
+++ react-folder/src/index.js
@@ -5,4 +5,22 @@ import ReactDOM from 'react-dom/client';
 // const root = ReactDOM.createRoot(document.getElementById('root'))
 // root.render(myfirstElement)
 
-ReactDOM.render(<p>My React Render HTML doc</p>, document.getElementById('root'))
+//. ReactDOM.render(<p>My React Render HTML doc</p>, document.getElementById('root'))
+
+
+const tableData = (
+    <table>
+        <tr>
+            <th>Classes</th>
+        </tr>
+        <tr>
+            <td>Class 2</td>
+        </tr>
+        <tr>
+            <td>Class 3</td>
+        </tr>
+    </table>
+)
+
+const root2 = ReactDOM.createRoot(document.getElementById('root'))
+root2.render(tableData)
