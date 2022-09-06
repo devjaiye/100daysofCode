a50d692de75fd0c3e89bdbf68f1b6c3655c7f6c6
devjaiye <dayojaiye74@gmail.com>
2022-09-06 16:34:09 +0100

    JSX conditional stmt using ternary - day 11

--- react-folder/src/index.js
+++ react-folder/src/index.js
@@ -2,15 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 
-//..if you are using multiple elements wrap them inside a parent <div> or fragment <>
-const multipleElements = (
-    <div>
-    <p>Hello world 1</p>
-    <p> Hello world 2</p>
-    </div>
-    )
-
-    ReactDOM.createRoot(document.getElementById('root')).render(multipleElements)
+ReactDOM.createRoot(document.getElementById('root'))
     
 
 
