c219448d829f9ce1306160098f2e06d4c3d95549
devjaiye <dayojaiye74@gmail.com>
2022-09-06 16:07:30 +0100

    JSX example code added - day 11

--- react-folder/src/index.js
+++ react-folder/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-//...the expression is wrapped in the curly braces { }
-const jsxWithExpression = <h1>I am {5 + 5} times better than before</h1>
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(jsxWithExpression)
+const myList = (
+    <ul>
+        <li>Rice</li>
+        <li>Beans</li>
+        <li>Garri</li>
+        <li>Maize</li>
+    </ul>
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(myList)
